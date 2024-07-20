8e7f3a69d447300e505c4a1affc83117b3e430bd
pr0m3th3usEx <thomas1.michel@epitech.eu>
2024-07-20 14:03:26 +0200

    args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::time::Duration;
+
+async fn run(duration: Duration) {
+    println!("Schedule duration: {:?}", duration);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+    let snd_arg = args.next().expect("Should have one argument");
+
+    match snd_arg.parse::<u64>() {
+        Ok(seconds) => smol::block_on(run(Duration::new(seconds, 0))),
+        Err(err) => panic!("{}", err),
+    }
 }
